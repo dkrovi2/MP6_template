3a90d032a71ff636644d43d6129bfdee4d546a1e
Sarma Krovi <sarmakdvsr@gmail.com>
2021-03-10 23:10:12 -0500

    TitleCountSpark.py

--- TitleCountSpark.py
+++ TitleCountSpark.py
@@ -5,26 +5,55 @@
 import sys
 from pyspark import SparkConf, SparkContext
 
+
+def splitline(line, delimiters):
+    source = line
+    for ch in delimiters:
+        # print("ch="+ch+", line="+line)
+        line = line.replace(ch, ' ')
+    target = line.split(' ')
+    # print("Source="+source+"\n"+"Target="+line)
+    return target
+
 stopWordsPath = sys.argv[1]
 delimitersPath = sys.argv[2]
 
+stopwords = []
+delimiters = []
+
 with open(stopWordsPath) as f:
-	#TODO
+    stopwords = [x.strip().lower() for x in f.readlines()]
 
 with open(delimitersPath) as f:
-    #TODO
+    delimiters = [x for x in f.readlines()]
+
 
 conf = SparkConf().setMaster("local").setAppName("TitleCount")
 conf.set("spark.driver.bindAddress", "127.0.0.1")
-sc = SparkContext(conf = conf)
+sc = SparkContext(conf=conf)
 
-lines = sc.textFile(sys.argv[3],1)
+lines = sc.textFile(sys.argv[3], 1)
+words = lines.flatMap(lambda line: splitline(line.lower(), delimiters[0]))\
+    .filter(lambda word: word not in stopwords)\
+    .filter(lambda word: word != '')\
+    .map(lambda word: (word, 1))\
+    .reduceByKey(lambda a, b: a + b)\
+    .map(lambda x: (x[1], x[0]))\
+    .sortByKey(False)
 
-#TODO
 
-outputFile = open(sys.argv[4],"w")
+results = []
 
-#TODO
-#write results to output file. Foramt for each line: (line +"\n")
+for entry in words.take(5):
+    result = list(entry)
+    results.append(result[1] + "\t" + str(result[0]))
+
+results.sort()
+outputFile = open(sys.argv[4], "w")
+
+for entry in results:
+    outputFile.write(entry + "\n")
+# TODO
+# write results to output file. Format for each line: (line +"\n")
 
 sc.stop()
