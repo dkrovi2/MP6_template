59648437f68b5c9cba5ee288935b682e4ca4f10b
Sarma Krovi <sarmakdvsr@gmail.com>
2021-03-11 13:16:29 -0500

    popularity league

--- PopularityLeagueSpark.py
+++ PopularityLeagueSpark.py
@@ -14,9 +14,47 @@ lines = sc.textFile(sys.argv[1], 1)
 
 leagueIds = sc.textFile(sys.argv[2], 1)
 
+def links(line):
+    tokens = line.split(':')
+    parent = tokens[0].strip()
+    children = tokens[1].strip().split(' ')
+    pages = []
+    for child in children:
+        child = child.strip()
+        if parent != child:
+            pages.append(child + "\t1")
+    pages.append(parent + "\t0")
+    return pages
+
+leagueIds = leagueIds.collect()
+
+popular = lines.flatMap(lambda line: links(line)) \
+    .map(lambda a: (a.split("\t")[0], int(a.split("\t")[1]))) \
+    .reduceByKey(lambda a, b: a + b) \
+    .filter(lambda entry: entry[0] in leagueIds)\
+    .map(lambda entry: (entry[1], entry[0])) \
+    .sortByKey() \
+    .map(lambda entry: (str(entry[1]), entry[0]))\
+    .collect()
+
+count = 0
+rank = 0
+nextrank = 0
+results = []
+for entry in popular:
+    if count != entry[1]:
+        rank = nextrank
+    count = entry[1]
+    nextrank = nextrank + 1
+    results.append((entry[0], rank))
+print(popular)
+print(results)
+results.sort()
 #TODO
 
 output = open(sys.argv[3], "w")
+for entry in results:
+    output.write(str(entry[0]) + "\t" + str(entry[1]) + "\n")
 
 #TODO
 #write results to output file. Foramt for each line: (key + \t + value +"\n")
